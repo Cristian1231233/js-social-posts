16d43a5a693722786fe58341cd0d26c77010d487
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-05 15:30:32 +0100

    struttura commit

--- main.js
+++ main.js
@@ -65,25 +65,25 @@ let html = '';
 // creo un ciclo for
 for( let i = 0; i < posts.length; i++){
     
-    const posts = posts[i];
-    console.log(posts[i]);
+    const post = posts[i];
+    console.log(post);
     html += 
     `
     <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                        <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author"></div>
+                        <div class="post-meta__author">${post.author.name}</div>
                         <div class="post-meta__time">4 mesi fa</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">${posts.content}</div>
+            <div class="post__text">${post.content}</div>
             <div class="post__image">
-                <img src="${posts.media}" alt="">
+                <img src="${post.media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -94,7 +94,7 @@ for( let i = 0; i < posts.length; i++){
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts.likes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
                     </div>
                 </div> 
             </div>            
